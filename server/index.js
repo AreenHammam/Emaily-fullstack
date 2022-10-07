49198940c9fcacf66c06235752f0b11bcf25616c
AreenHammam <hammam3reen@gmail.com>
2022-10-07 21:36:06 +0300

    server

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+// require('./models/User');
+// require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+// mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.get('/',(req,res)=>{
+    res.send("hi")
+})
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
